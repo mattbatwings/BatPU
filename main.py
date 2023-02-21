effe37e96a0f21aaf6a77625a340d63b0b917cd8
Matt <69316972+mattbatwings@users.noreply.github.com>
2023-02-21 01:22:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import mcschematic
+from assembler import assemble
+from simulator import simulate
+from schematic import make_schematic
+
+# for schematic
+path = 'C:/Users/12483/AppData/Roaming/.minecraft/config/worldedit/schematics'
+name = 'program'
+version = mcschematic.Version.JE_1_18_2
+
+def assemble_to_schematic(assembly_filename):
+    global path, name, version
+    machine_code_layer = 'output.mc'
+    assemble(assembly_filename, machine_code_layer)
+    make_schematic(machine_code_layer, path, name, version)
+
+def assemble_and_simulate(assembly_filename):
+    machine_code_layer = 'output.mc'
+    assemble(assembly_filename, machine_code_layer)
+    simulate(machine_code_layer)
+
+def main():
+    program = 'programs/collatz.as'
+    
+    # assemble_to_schematic(program)
+    assemble_and_simulate(program)
+
+if __name__ == "__main__":
+    main()
